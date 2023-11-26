db87485e981a18ae9428581b0e3fad15b66ffcac
Muhammad <muhammad.installer@gmail.com>
2023-11-26 08:04:30 +0600

    connect and bring data from api by rxjs, asyncronous with observable

--- src/my-front/src/app/app.component.ts
+++ src/my-front/src/app/app.component.ts
@@ -4,24 +4,23 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
 import { CourseComponent } from './components/course/course.component';
 import { ICourse } from './data/ICourse';
-import { CourseService } from './services/course.service';
+import { CourseService } from './services/course.service';   
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonComponent, CourseComponent],
+  imports: [CommonModule, RouterOutlet, ButtonComponent, CourseComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-front';
   courses: ICourse[] = [];
+  private username: string = "muhammad.installer@gmail.com";
+  private password: string = "faisal123";
 
   constructor(private courseService: CourseService){ }
 
-  update(){
-	this.courses = this.courseService.getCourses();
-  }
 
   onClick(){
     console.log("button clicked");
@@ -29,5 +28,14 @@ export class AppComponent {
    onClick2(){
     console.log("button clicked2");
    }
-  
+
+  update() {
+    this.courseService.getToken(this.username, this.password).subscribe((token: string) => {
+      this.courseService
+        .getCourses(token)
+        .subscribe((data: ICourse[]) => {
+          this.courses = data;
+        });
+    });
+  }
 }
--- src/my-front/src/app/services/course.service.ts
+++ src/my-front/src/app/services/course.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ICourse } from '../data/ICourse'
-import { Course } from '../data/Course';
-import { count } from 'console';
+import { ICourse } from '../data/ICourse' 
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  private tokenUrl : string = "/v3/token";
+  private dataUrl: string = "/v3/course";
 
-  getCourses() : ICourse[]{
-    return [
-      new Course({ name : "C#", fees : 8000 }),
-      new Course({ name : "Asp.net", fees : 30000 }),
-      new Course({name : "laravel", fees: 12000})
-    ];
-    }  
+  constructor(private http: HttpClient) { }
+
+  getToken(username: string, password: string) : Observable<string>{
+    let token = this.http.get(this.tokenUrl +
+      `?email=${username}&password=${password}`,
+        {
+          headers: {'Accept':'text/html, application/xhtml+xml, */*','Content-Type':'application/x-www-form-urlencoded'},
+          responseType: 'text'
+        }
+      );
+  
+      return token;
+    }
+
+    getCourses(token: string) : Observable<ICourse[]>{
+
+      return this.http.get<ICourse[]>(this.dataUrl,
+        {
+          headers: {'Authorization':`Bearer ${token}`}
+        },
+      );
+    }
 }
